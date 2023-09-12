71484fcda634a82f68ccb1b51f82c0c6f5ee29ae
Vaishnavi Veerkumar <144850845+vaishveer@users.noreply.github.com>
2023-09-12 19:23:25 -0400

    Add files via upload

---
+++ LAB1/Lab_1/src/model/Product.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author vaish
+ */
+public class Product {
+    private String name;
+    private String descp;
+    private String availNum;
+    private String price;
+    
+    //getters and setters to access the attributes
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescp() {
+        return descp;
+    }
+
+    public void setDescp(String descp) {
+        this.descp = descp;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+}
